b62782f5621e4b0268b4a78aec0eca9169b3a3e9
Your Name <you@example.com>
2026-09-15 22:41:24 +0700

    init

--- src/components/admin/HeaderAdmin.jsx
+++ src/components/admin/HeaderAdmin.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const HeaderAdmin = () => { 
     return ( 
         <header className='bg-teal-600 h-16 flex items-center px-6'> 
-            Header 
- 
+            
         </header> 
     ) 
 } 
--- src/components/admin/SidebarAdmin.jsx
+++ src/components/admin/SidebarAdmin.jsx
@@ -59,7 +59,9 @@ const SidebarAdmin = () => {
             </nav> 
  
             <div> 
-                <NavLink className={({ isActive }) => 
+                <NavLink 
+                to={'/Home'}
+                className={({ isActive }) => 
                     isActive 
                         ? 'bg-cyan-600 rounded-md text-white px-4 py-2 flex items-center' 
                         : 'text-teal-200 px-4 py-2 hover:bg-teal-800 hover:text-cyan-300 rounded flex items-center' 
